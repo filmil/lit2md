1477844c47cd4f0caf362709b400af73f01f3e17
Filip Filmar <filmil@gmail.com>
2025-09-18 22:10:08 -0700

    feat: initial release, let's see

--- cmd/lit2md/main.go
+++ cmd/lit2md/main.go
@@ -4,10 +4,144 @@
 package main
 
 import (
+	"bufio"
 	"flag"
+	"fmt"
+	"io"
 	"log"
 	"os"
+	"strings"
 )
+
+type State int
+
+const (
+	StateNone State = iota
+	StateCode
+	StateText
+)
+
+type DocComment struct {
+	docPrefix, docPrefix2 string
+}
+
+func NewDocComment(commentStr string) DocComment {
+	// Used to delineate doc prefixes
+	docPrefix := fmt.Sprintf("%s]", commentStr)
+	docPrefix2 := fmt.Sprintf("%s] ", commentStr)
+	return DocComment{
+		docPrefix:  docPrefix,
+		docPrefix2: docPrefix2,
+	}
+}
+
+func (self *DocComment) IsPrefixOf(str string) bool {
+	strTrim := strings.TrimLeft(str, "\t ")
+	return strings.HasPrefix(strTrim, self.docPrefix) || strings.HasPrefix(strTrim, self.docPrefix2)
+}
+
+func (self *DocComment) UnapplyPrefix(str string) string {
+	strTrim := strings.TrimLeft(str, "\t ")
+
+	after, cut := strings.CutPrefix(strTrim, self.docPrefix2)
+	if cut {
+		return after
+	}
+	after, cut = strings.CutPrefix(strTrim, self.docPrefix)
+	if cut {
+		return after
+	}
+	return strings.TrimRight(strTrim, "\n\r")
+}
+
+func convert(in io.Reader, out io.Writer, commentStr, lang string) error {
+	d := NewDocComment(commentStr)
+	state := StateNone
+	scanner := bufio.NewScanner(in)
+	scanner.Split(bufio.ScanLines)
+
+	_ = out
+
+	for scanner.Scan() {
+		t := scanner.Text()
+		np := d.UnapplyPrefix(t)
+
+	l:
+
+		switch state {
+		case StateNone:
+			trimmed := strings.Trim(t, "\n\t ")
+			if trimmed != "" {
+				// It's something, check what.
+				if d.IsPrefixOf(t) {
+					state = StateText
+				} else {
+					fmt.Fprintf(out, "```%s\n", lang) // Begin code
+					state = StateCode
+				}
+				goto l
+			}
+			// Otherwise, it's empty string, so read next line.
+		case StateCode:
+			if d.IsPrefixOf(t) {
+				// Incoming text line!
+				fmt.Fprintf(out, "```\n") // End code
+				fmt.Fprintf(out, "%v\n", np)
+				// TODO: identify file type.
+				state = StateText
+			} else {
+				// Incoming another code line!
+				// Output verbatim.
+				fmt.Fprintf(out, "%v\n", t)
+			}
+		case StateText:
+			if d.IsPrefixOf(t) {
+				// Still text.
+				fmt.Fprintf(out, "%v\n", np)
+			} else {
+				trimmed := strings.Trim(t, "\n\t ")
+				if trimmed != "" {
+					// It's code now!
+					fmt.Fprintf(out, "\n```%s\n", lang) // Begin code
+					fmt.Fprintf(out, "%v\n", np)
+					state = StateCode
+				}
+			}
+		default:
+			panic(fmt.Sprintf("unexpected main.State: %#v", state))
+		}
+	}
+
+	// If we end in the "code" state, close the code block.
+	if state == StateCode {
+		fmt.Fprintf(out, "```\n")
+	}
+	return nil
+}
+
+func run(inputFilename, outputFilename string) error {
+	if inputFilename == "" {
+		return fmt.Errorf("flag --input= is mandatory")
+	}
+
+	in, err := os.Open(inputFilename)
+	if err != nil {
+		return fmt.Errorf("error while opening: %q: %v", inputFilename, err)
+	}
+	defer in.Close()
+
+	if outputFilename == "" {
+		return fmt.Errorf("flag --output=... is mandatory")
+	}
+
+	out, err := os.Open(outputFilename)
+	if err != nil {
+		return fmt.Errorf("error while opening: %q: %v", outputFilename, err)
+	}
+	defer out.Close()
+
+	return convert(in, out, "--", "vhdl")
+}
 
 func main() {
 
@@ -19,13 +153,8 @@ func main() {
 	flag.StringVar(&outputFilename, "output", "", "output filename (markdown)")
 	flag.Parse()
 
-	if inputFilename == "" {
-		log.Printf("flag --input= is mandatory")
+	if err := run(inputFilename, outputFilename); err != nil {
+		log.Printf("error: %v", err)
 		os.Exit(1)
-	}
-
-	in, err := os.Open(inputFilename)
-	if err != nil {
-		log.Printf("error while opening: %q: %v", inputFilename, err)
 	}
 }
--- cmd/lit2md/main_test.go
+++ cmd/lit2md/main_test.go
@@ -2,8 +2,109 @@
 
 package main
 
-import "testing"
+import (
+	"strings"
+	"testing"
+)
 
 func TestOne(t *testing.T) {
-	// Always pass.
+	t.Parallel()
+	tests := []struct {
+		summary  string
+		in       string
+		expected string
+	}{
+		{"empty", "", ""},
+		{"basic", "Code", "```vhdl\nCode\n```\n"},
+		{
+			summary: "basic mixed",
+			in: strings.Join([]string{
+				"--] # Hello world",
+				"--] This is text",
+				"This is code.",
+			}, "\n"),
+			expected: strings.Join([]string{
+				"# Hello world",
+				"This is text",
+				"",
+				"```vhdl",
+				"This is code.",
+				"```\n",
+			}, "\n"),
+		},
+		{
+			summary: "empty line before code",
+			in: strings.Join([]string{
+				"",
+				"--] # Hello world",
+				"--] This is text",
+				"This is code.",
+			}, "\n"),
+			expected: strings.Join([]string{
+				"# Hello world",
+				"This is text",
+				"",
+				"```vhdl",
+				"This is code.",
+				"```\n",
+			}, "\n"),
+		},
+		{
+			summary: "empty line befor code in the middle",
+			in: strings.Join([]string{
+				"",
+				"--] # Hello world",
+				"--] This is text",
+				"",
+				"This is code.",
+			}, "\n"),
+			expected: strings.Join([]string{
+				"# Hello world",
+				"This is text",
+				"",
+				"```vhdl",
+				"This is code.",
+				"```\n",
+			}, "\n"),
+		},
+		{
+			summary: "empty line befor code in the middle",
+			in: `This is code.
+
+--] This is text
+
+This is code again.
+`,
+			expected: strings.Join([]string{
+				"```vhdl",
+				"This is code.",
+				"",
+				"```",
+				"This is text",
+				"",
+				"```vhdl",
+				"This is code again.",
+				"```\n",
+			}, "\n"),
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.summary, func(t *testing.T) {
+			inRead := strings.NewReader(test.in)
+			var outWrite strings.Builder
+
+			if err := convert(inRead, &outWrite, "--", "vhdl"); err != nil {
+				t.Fatalf("error while reading: %v", err)
+			}
+
+			actual := outWrite.String()
+
+			if test.expected != actual {
+				t.Errorf("want:\n%v, got:\n%v", test.expected, actual)
+			}
+
+		})
+	}
 }
